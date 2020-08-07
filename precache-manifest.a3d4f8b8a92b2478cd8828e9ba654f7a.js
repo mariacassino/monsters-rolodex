60a72302fca68420ce31b56ba2b0a94839c749ad
Maria Cassino <maria.cassino@mckesson.com>
2020-08-07 16:11:03 -0400

    Updates

---
+++ precache-manifest.a3d4f8b8a92b2478cd8828e9ba654f7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0c09fb0294edd47a5336f8d38e9eae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5fcb312d3c6c494eae64",
+    "url": "/monsters-rolodex/static/css/main.d3068bf5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fcb312d3c6c494eae64",
+    "url": "/monsters-rolodex/static/js/main.fb5ba127.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
